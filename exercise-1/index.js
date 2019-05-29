95bec59a385467634840e7074e36ff8d6add6f3d
Victor DePina <depinav@gmail.com>
2019-05-28 23:57:20 -0400

    Updated port

--- exercise-1/index.js
+++ exercise-1/index.js
@@ -4,9 +4,9 @@ const bodyParser = require('body-parser');
 
 const router = require('./router/router');
 
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use('/', router);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
